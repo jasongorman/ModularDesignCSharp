2c66a7d686625d259b2ffc365410d9c5d9a14a67
Jason Gorman <jason.gorman@codemanship.com>
2020-07-29 09:18:56 +0100

    Changed FittedPrice() params to fields set in constructor

--- ModularDesign/CarpetQuote.cs
+++ ModularDesign/CarpetQuote.cs
@@ -4,14 +4,27 @@ namespace ModularDesign
 {
     public class CarpetQuote
     {
-        public double FittedPrice(double roomLength, double roomWidth, double pricePerSqMtr, bool roundUp)
-        {
-            double area = roomLength * roomWidth;
+        private readonly double _roomLength;
+        private readonly double _roomWidth;
+        private readonly double _pricePerSqMtr;
+        private readonly bool _roundUp;
 
-            if (roundUp)
+        public CarpetQuote(double roomLength, double roomWidth, double pricePerSqMtr, bool roundUp)
+        {
+            _roomLength = roomLength;
+            _roomWidth = roomWidth;
+            _pricePerSqMtr = pricePerSqMtr;
+            _roundUp = roundUp;
+        }
+        
+        public double FittedPrice()
+        {
+            double area = _roomLength * _roomWidth;
+
+            if (_roundUp)
                 area = Math.Ceiling(area);
             
-            return area * pricePerSqMtr;
+            return area * _pricePerSqMtr;
         }
     }
 }
